bcc366253f4e481171545334644ee3160dae2f92
Attila Lénárt <meepkeeper@gmail.com>
2022-11-10 10:47:42 +0100

    - dev

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # billzon-php-sdk
+
+billzon-php-sdk is a PHP library for [billzon ERP](https://www.billzon.com/).
+
+## Usage
+
+```php
+require 'sdk/Base_api_model.php';
+require 'sdk/Booking_model.php';
+
+$booking_client = new Booking_model(array(
+	"api_key" => "REPLACE_WITH_YOUR_API_KEY"
+));
+
+$booking_list = $booking_client->get_list(array(
+	"list_offset" => 0,
+	"list_condition" => array(
+		// conditions
+	),
+));
+print_r($booking_list);
+```
+
+Please find the API endpoints [here](https://www.billzon.com/help-center/dokumentacio).
+
+## Contributing
+
+For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## FAQ
+[Terms and Conditions](https://www.billzon.com/dokumentum/aszf)
+
+[Privacy Policies](https://www.numinc.com/dokumentum/adatvedelem)
---
+++ sdk/Booking_model.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Booking
+ *
+ * @link https://www.billzon.com/help-center/dokumentacio/keszletkezeles-2/raktari-bevet-6
+ * @author info@numinc.com
+**/
+
+class Booking_model extends Base_api_model {
+
+    public function __construct($config = array()) {
+		$this->set_api_key($config["api_key"]);
+    }
+
+	public function get($params = array()){
+        try {
+            $object = new stdClass();
+			
+			if($params != NULL && isset($params['id']) && $params['id'] > 0){
+				$object = $this->call('GET', $this->get_api_endpoint().'booking/get', $params);
+			}
+
+            return $object;
+        } catch(Exception $ex) {  
+        }  
+          
+        return FALSE;  
+	}
+
+	public function get_list($params = array()){
+        try {            
+            return $this->call('GET', $this->get_api_endpoint().'booking/list', $params);
+        } catch(Exception $ex) {  
+        }  
+          
+        return FALSE;  
+	}
+
+	function save($params = array()){
+        try {
+            return $this->call('POST', $this->get_api_endpoint().'booking/save', $params);
+        } catch(Exception $ex) {  
+        }  
+          
+        return FALSE;
+	}
+
+	function delete($params = array()){
+		try {            
+            return $this->call('POST', $this->get_api_endpoint().'booking/delete', $params);
+        } catch(Exception $ex) {  
+        }  
+          
+        return FALSE;  
+	}
+
+	function close($params = array()){
+		try {            
+            return $this->call('POST', $this->get_api_endpoint().'booking/close', $params);
+        } catch(Exception $ex) {  
+        }  
+          
+        return FALSE;  
+	}
+	
+	function download($params = array()){
+		try {            
+			return $this->call('GET', $this->get_api_endpoint().'booking/download', $params);	
+        } catch(Exception $ex) {  
+        }  
+          
+        return FALSE;  
+	}
+}
+
+
