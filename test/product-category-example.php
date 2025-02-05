9158f1219fc4545f6ea96034cf67ad5a573e6e50
attilalenart <attila.lenart@numinc.com>
2025-02-05 21:59:20 +0100

    - fix

--- test/product-category-example.php
+++ test/product-category-example.php
@@ -12,3 +12,21 @@ $product_category_list = $product_category_client->get_list(array(
 ));
 
 print_r($product_category_list);
+
+// termék kategória rögzítés
+$product_category_save = $product_category_client->save(array(
+    "data" => array(
+		// "id" => "", // for update
+		"name_key" => "egyedikulcs", // 32 karakteres kulcs hivatkozás
+		"szjvtsz" => "", // SZJ/VTSZ  szám
+		"parent_category_id" => "", // Szülő kategória id
+		"category_cash_register_show" => "1", // Pénztárgép szűrőben megjelenik
+		"category_product_list_show" => "1", // Általános szűrőben megjelenik
+		"category_web_url" => "", // webshop kategória URL
+		"data" => array(
+			1 => "Termék kategória megnevezés" // language_id => adott nyelv megnevezése
+		),
+	),
+));
+
+print_r($product_category_save);
