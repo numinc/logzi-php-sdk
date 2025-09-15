706e340daf1000ba735f138d52773c36a40993d2
attilalenart <attila.lenart@numinc.com>
2025-09-15 10:47:43 +0200

    - Reserve added

---
+++ src/Reserve_model.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Reserve
+ *
+ * @author info@numinc.com
+**/
+
+namespace Numinc\Logzi;
+
+class Reserve_model extends Base_api_model {
+
+    public function __construct($config = array()) {
+		$this->set_api_key($config["api_key"]);
+    }
+
+	public function get($params = array()){
+        try {
+            return $this->call('GET', $this->get_api_endpoint().'reserve/get', $params);
+        } catch(\Exception $ex) {  
+        }  
+          
+        return FALSE;  
+	}
+
+	public function get_list($params = array()){
+        try {            
+            return $this->call('GET', $this->get_api_endpoint().'reserve/list', $params);
+        } catch(\Exception $ex) {  
+        }  
+          
+        return FALSE;  
+	}
+
+	function save($params = array()){
+        try {
+            return $this->call('POST', $this->get_api_endpoint().'reserve/save', $params);
+        } catch(\Exception $ex) {  
+        }  
+          
+        return FALSE;
+	}
+
+	function delete($params = array()){
+		try {            
+            return $this->call('POST', $this->get_api_endpoint().'reserve/delete', $params);
+        } catch(\Exception $ex) {  
+        }  
+          
+        return FALSE;  
+	}
+
+	function close($params = array()){
+		try {            
+            return $this->call('POST', $this->get_api_endpoint().'reserve/close', $params);
+        } catch(\Exception $ex) {  
+        }  
+          
+        return FALSE;  
+	}
+	
+	function download($params = array()){
+		try {            
+			return $this->call('GET', $this->get_api_endpoint().'reserve/download', $params);	
+        } catch(\Exception $ex) {  
+        }  
+          
+        return FALSE;  
+	}
+}
+
+
---
+++ test/reserve-example.php
@@ -0,0 +1,48 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$reserve_client = new Numinc\Logzi\Reserve_model(array(
+	"api_key" => "REPLACE_WITH_YOUR_API_KEY"
+));
+
+// hardcode végpont
+$reserve_save = $reserve_client->save(array(
+    "data" => array(
+        "company_id" => 1, // kiállító cég azonosító (mindig 1)
+        "company_user_id" => 1, // kiállító cég felhasználó azonosító (1-es értékkel a system user hozza létre, ezt használjuk az automatizmusokhoz)
+        "company_address_id" => 1, // kiállító cég szállítási címe
+        "date_perform" =>  "2023-10-08", // teljesítés kelt
+        "partner_id" => 0, // vevő cég azonosítója
+        "partner_user_id" => 0, // vevő cég kapcsolattartó azonosítója
+        "currency_id" => 1, // pénznem azonosítója
+        "currency_exchange" => 0.00, // pénznem árfolyama, amennyiben eltér az alap devizától
+        "comment_top" =>  "", // ügyfél megjegyzés
+        "comment_bottom" =>  "", // belső megjegyzés
+        "jobnumber_id" =>  "", // munkaszám azonosító
+        "departmentnumber_id" =>  "", // részlegszám azonosító
+        "project_id" =>  "", // projekt azonosító
+
+        "item" => array(
+            0 => array(
+                "product_id" => 2, // termék vagy szolgáltatás azonosító
+                "store_id" => 1, // telephely azonosító
+                "quantity" => 1, // mennyiség
+                "tax_id" => 1, // tétel áfakulcs
+                "comment_top" => "", // tétel megjegyzés
+                "price" => 150.00 // tétel ára
+            ),
+        ),
+    )
+));
+
+print_r($reserve_save);
+
+// hardcode lezárás
+$reserve_close = $reserve_client->close(array(
+    "data" => array(
+        "receipt_id" => 0, // bizonylat azonosító ( fenti példát követve $reserve_save->data->id )
+    )
+));
+
+print_r($reserve_close);
